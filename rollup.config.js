606d0ea5095437ebdc655d7d874bad3f3fd74f59
Xiou.Wang <xiou.wang@feisu.com>
2024-07-16 15:43:47 +0800

    feat: d.ts调整

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,39 @@
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 const name = "create-enum";
 
-export default {
-  input: "./src/index.ts",
-  output: [
-    {
-      file: `./dist/${name}.cjs.js`,
-      format: "cjs",
-    },
-    {
-      file: `./dist/${name}.es.js`,
-      format: "es",
-    },
-    {
-      file: `./dist/${name}.umd.js`,
-      format: "umd",
-      name: "$Enum",
-    },
-  ],
-  plugins: [typescript({ tsconfig: "./tsconfig.json" })],
-};
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: `./dist/${name}.cjs.js`,
+        format: "cjs",
+      },
+      {
+        file: `./dist/${name}.es.js`,
+        format: "es",
+      },
+      {
+        file: `./dist/${name}.umd.js`,
+        format: "umd",
+        name: "$Enum",
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json", //导出声明文件
+        declaration: true,
+        //类型目录
+        declarationDir: "types",
+        //输出目录
+        outDir: "dist",
+      }),
+    ],
+  }, // 第二步将esm打包出的文件再打包到index.d.ts中去
+  {
+    input: "./dist/types/index.d.ts",
+    output: [{ file: "./dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
